2e6cfa90f95662f7561045d14d3c3e4efdd5a591
Anna-Klara <58220090+AnnisNilsson@users.noreply.github.com>
2021-11-11 20:04:46 +0100

    commit

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -18,11 +18,11 @@ const theme = createMuiTheme({
   typography: {
     fontFamily: ['Comfortaa'],
     h1: {
-      fontSize: '2rem',
+      fontSize: '1.4rem',
       fontWeight: 700,
     },
     h2: {
-      fontSize: '1.7rem',
+      fontSize: '1.2rem',
       fontWeight: 500,
     },
     h3: {
